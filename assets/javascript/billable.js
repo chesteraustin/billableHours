82188fd0be9fb8c99fffa23804fd50510c9e46e9
DanielFMurillo <danielfmurillo@yahoo.com>
2017-05-20 11:00:02 -0700

    initial push

---
+++ assets/javascript/billable.js
@@ -0,0 +1,18 @@
+
+$(document).ready(function(){
+  console.log("ready");
+
+
+
+
+
+$("#add-user").on("click", function() {
+		console.log("click");
+	
+		/*$("#name-input").text()
+		$("#role-input").text()
+		$("#start-date-input").text()
+		$("monthly-rate-input").text()*/
+
+};
+
