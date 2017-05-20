8af5b85916d0935b198b93105e9fd697020a6990
LmLilly <lillym@Lidiettes-MacBook-Pro.local>
2017-05-20 11:31:15 -0700

    changes

--- assets/javascript/billable.js
+++ assets/javascript/billable.js
@@ -2,29 +2,21 @@
 $(document).ready(function(){
   console.log("ready");
 
-
-var name = $("#name-input").val();
-var role = $("#role-input").val();
-var startDate = $("#start-date-input").val();
-var monthlyRate = $("monthly-rate-input").val();
-
-
-
-$("#add-user").on("click", function() {
-	event.preventDefault();
+	$("#add-user").on("click", function() {
 		console.log("click");
-	
-	
+
+		var employeeName = $("#name-input").val();
+		var monthlyRate = $("#monthly-rate-input").val();
+		var role = $("#role-input").val();
+		var startDate = $("#start-date-input").val();
+
+		datebase.ref().push({
+			"employeeName": employeeName,
+			"monthlyRate": monthlyRate,
+			"role": role,
+			"startDate": startDate
+			
+		});
+	});
 });
-
-
-datebase.ref().push({
-	name: name,
-	role: role,
-	startDate: startDate,
-	monthlyRate: monthlyRate,
-
-
-});
-
 
