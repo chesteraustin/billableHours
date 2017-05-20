5ed54f377e9041c05e643b5889b19f77e481e3c5
DanielFMurillo <danielfmurillo@yahoo.com>
2017-05-20 11:13:13 -0700

    push

--- assets/javascript/billable.js
+++ assets/javascript/billable.js
@@ -3,16 +3,28 @@ $(document).ready(function(){
   console.log("ready");
 
 
+var name = $("#name-input").val();
+var role = $("#role-input").val();
+var startDate = $("#start-date-input").val();
+var monthlyRate = $("monthly-rate-input").val();
 
 
 
 $("#add-user").on("click", function() {
+	event.preventDefault();
 		console.log("click");
 	
-		/*$("#name-input").text()
-		$("#role-input").text()
-		$("#start-date-input").text()
-		$("monthly-rate-input").text()*/
+	
+});
+
+
+datebase.ref().push({
+	name: name,
+	role: role,
+	startDate: startDate,
+	monthlyRate: monthlyRate,
+
 
 });
+
 
